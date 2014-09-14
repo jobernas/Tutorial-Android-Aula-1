ff0b85930c9c5eb2d1e24d8eb4101cdae5ba72a4
Joao Luis <joao.luis@loopbug.com>
2014-09-14 21:54:40 +0100

    Adicionado TextView para gerar o Output do "Hello World"

---
+++ src/com/loopbug/aula1_helloworld/MainActivity.java
@@ -0,0 +1,41 @@
+package com.loopbug.aula1_helloworld;
+
+import com.loopbug.aula1_helloworld.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtOutput = (TextView)findViewById(R.id.txt_output_text);
+        txtOutput.setText("Hello World");
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
